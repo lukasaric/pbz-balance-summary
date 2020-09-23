2d0713d63281d9c7379ec6b14cbdc44ed2d43c4f
Luka Saric <rpg.ari7@gmail.com>
2020-09-23 18:39:40 +0200

    Handler cleanup :lipstick:

--- handler.js
+++ handler.js
@@ -8,9 +8,8 @@ module.exports.resolveAccBalance = async event => {
   const encodedContent = event.Records[0].Sns.Message.content;
   const email = await simpleParser(encodedContent);
   const reports = adjustAttachments(email);
-  const accBalance = await new AccBalanceResolver(reports).inferBalance();
-  forwardReport(accBalance);
-  return { statusCode: 200 };
+  return new AccBalanceResolver(reports).inferBalance()
+    .then(total => forwardReport(total));
 };
 
 function adjustAttachments({ attachments }) {
