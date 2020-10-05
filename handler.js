c7608945b886485272fdf095d3fcb76ea93e2ece
Luka Saric <rpg.ari7@gmail.com>
2020-10-05 07:43:35 +0200

    Cleanup :surfer:

--- handler.js
+++ handler.js
@@ -8,8 +8,7 @@ const AttachmentsResolver = require('./attachmentsResolver');
 module.exports.resolveAccBalance = async event => {
   const mail = event.Records[0].ses.mail;
   if (!isAllowedSource(mail)) return;
-  const files = await storage.listFiles();
-  const opts = { files, incomingKey: mail.messageId };
+  const opts = { files: await storage.listFiles(), incomingKey: mail.messageId };
   const attachments = await new AttachmentsResolver(opts).getAttachments();
   if (isEmpty(attachments)) return;
   return new AccBalanceResolver(attachments).inferBalance()
